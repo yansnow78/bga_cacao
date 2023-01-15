b09fc2131f627b3cf026498236055f093d588e91
yansnow78 <ygrugni@gmail.com>
2023-01-15 01:04:28 +0100

    pinch zoom is now centered on the middle of the 2 fingers

--- modules/js/scrollmapWithZoom.js
+++ modules/js/scrollmapWithZoom.js
@@ -41,7 +41,7 @@ define([
                 this.scrollto(this.board_x,this.board_y, 0, 0);
                 //console.log("onResize");
             },
-            findEventIndex: function (event) {
+            findPointerIndex: function (event) {
                 let i = this.pointers.length
                 while (i--) {
                     if (this.pointers[i].pointerId === event.pointerId) {
@@ -51,18 +51,59 @@ define([
                 return -1
             },
             addPointer: function (event) {
-                const i = this.findEventIndex(event)
+                const i = this.findPointerIndex(event)
                 // Update if already present
                 if (i > -1) {
-                    this.pointers.splice(i, 1)
-                }
-                this.pointers.push(event)
+                    var prevEv=this.pointers[i];
+                    this.pointers.splice(i, 1, event);
+                    return prevEv; 
+                } else
+                    this.pointers.push(event)
             },
             removePointer: function (event) {
-                const i = this.findEventIndex(event)
+                const i = this.findPointerIndex(event)
                 if (i > -1) {
                     this.pointers.splice(i, 1)
                 }
+            },
+            getPointerPrevEvent: function (event) {
+                const i = this.findPointerIndex(event)
+                if (i > -1) {
+                    return this.pointers[i]
+                }
+            },
+            getXYCoord: function (ev, ev2) {
+                var width = dojo.style(this.container_div, "width");
+                var height = dojo.style(this.container_div, "height");
+                var containerRect=this.container_div.getBoundingClientRect();
+                // var scale = (containerRect.right-containerRect.left) / this.container_div.offsetWidth;
+                // var scale2 = width / this.container_div.offsetWidth;
+                // console.log(scale, scale2);
+                var clientX=ev.clientX;
+                var clientY=ev.clientY;
+                if (typeof ev2 !== 'undefined'){
+                    clientX=(clientX+ev2.clientX)/2;
+                    clientY=(clientY+ev2.clientY)/2;
+                }
+
+                // if ( /chrome/i.test( navigator.userAgent ) ) {
+                //     // In Chrome zoom of a parent node requires to adjust mouseX/mouseY
+                //     var parent = this.container_div.parentNode;
+                //     while ( parent.parentNode != null ) {
+                //         if ( parent.style.zoom && parent.style.zoom != 1 ) {
+                //             clientX /= parent.style.zoom;
+                //             clientY /= parent.style.zoom;
+                //             break;
+                //         }
+                //         parent = parent.parentNode;
+                //     }
+                // }
+                
+                // console.log(clientX,clientY);
+                // console.log(ev.pageX,ev.pageY);
+                var x=clientX-containerRect.x-width/2;
+                var y=clientY-containerRect.y-height/2;
+                return [x,y];
             },
             startDragging: function (ev) {
                 if (!this.bEnableScrolling)
@@ -73,28 +114,36 @@ define([
                 this.pointer_startY = ev.pageY;
             },
             onPointerDown: function (ev) {
+                // console.log(ev.pageX+' '+ev.pageY);
                 if (!this.bEnableScrolling && !this.bEnableZooming)
                     return;
                 if (this.pointers.length == 0) {
                     this.onpointermove_handler = dojo.connect(document, "onpointermove", this, "onPointerMove");
                     this.onpointerup_handler = dojo.connect(document, "onpointerup", this, "onPointerUp");
                     this.onpointercancel_handler = dojo.connect(document, "onpointercancel", this, "onPointerUp");
-                    this.startDragging(ev);
+                    //this.startDragging(ev);
                 } else
                     this.isdragging = false;
                 this.addPointer(ev);
             },
             onPointerMove: function (ev) {
                 ev.preventDefault();
-                this.addPointer(ev);
+                //var prevEv = this.getPointerPrevEvent(ev);
+                var prevEv =  this.addPointer(ev);
 
                 // If one pointer is move, drag the map
                 if (this.pointers.length === 1) {
-
-                    if (this.isdragging) {
-                        this.scroll(ev.pageX - this.pointer_startX, ev.pageY - this.pointer_startY, 0, 0)
+                    if (!this.bEnableScrolling)
+                        return;
+                    if ((typeof prevEv !== 'undefined')) {
+                        // console.log(prevEv.pageX+' '+prevEv.pageY);
+                        //this.scroll(ev.pageX - this.pointer_startX, ev.pageY - this.pointer_startY, 0, 0)
+                        var [x,y] = this.getXYCoord(ev);
+                        var [xPrev,yPrev] = this.getXYCoord(prevEv);
+                        this.scroll(x  - xPrev , y - yPrev, 0, 0)
+                        //this.scroll(ev.screenX  - prevEv.screenX , ev.screenY - prevEv.screenY, 0, 0)
                     }
-                    this.startDragging(ev);
+                    //this.startDragging(ev);
                 }
                 // If two pointers are move, check for pinch gestures
                 else if (this.pointers.length === 2) {
@@ -110,15 +159,19 @@ define([
                         Math.pow(Math.abs(event2.clientX - event1.clientX), 2) +
                         Math.pow(Math.abs(event2.clientY - event1.clientY), 2)
                     );
-
+                    var [x,y] = this.getXYCoord(event1, event2);
                     if (this.prevDist > 0.0) {
                         const diff = curDist - this.prevDist;
-                        newZoom = this.zoom * (1 + this.zoomPinchDelta * diff);
-                        this.setMapZoom(newZoom);
+                        // newZoom = this.zoom * (1 + this.zoomPinchDelta * diff);
+                        newZoom = this.zoom * (curDist / this.prevDist);
+                        this.setMapZoom(newZoom, x, y);
+                        this.scroll(x  - this.xPrev , y - this.yPrev, 0, 0)
                     }
 
                     // Cache the distance for the next move event
                     this.prevDist = curDist;
+                    this.xPrev = x;
+                    this.yPrev = y;
                 }
                 dojo.stopEvent(ev);
             },
@@ -130,7 +183,6 @@ define([
                     dojo.disconnect(this.onpointermove_handler);
                     dojo.disconnect(this.onpointerup_handler);
                     dojo.disconnect(this.onpointercancel_handler);
-                    //console.log(this.board_x+' '+this.board_y)
                 }
 
                 // If the number of pointers down is less than two then reset diff tracker
@@ -142,11 +194,7 @@ define([
                 if (!this.bEnableZooming)
                     return;
                 evt.preventDefault();
-                var width = dojo.style(this.container_div, "width");
-                var height = dojo.style(this.container_div, "height");
-                var containerRect=this.container_div.getBoundingClientRect();
-                var x=evt.clientX-containerRect.x-width/2;
-                var y=evt.clientY-containerRect.y-height/2;//this.container_div.offsetHeight
+                var [x,y]=this.getXYCoord(evt);
                 this.changeMapZoom(evt.deltaY * -this.zoomWheelDelta, x, y);
             },
 
