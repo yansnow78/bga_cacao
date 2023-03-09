9676fdc2ba419caeb388b41fe4be8a33773a7974
yansnow78 <ygrugni@gmail.com>
2023-03-09 11:32:29 +0100

    further correct tooltip positioning

--- modules/js/gamegui_patch.js
+++ modules/js/gamegui_patch.js
@@ -11,74 +11,40 @@ function (dojo, declare) {
     return declare("ebg.core.gamegui_patch", null, {
         constructor: function(){
             console.log('ebg.core.gamegui_patch constructor');
+            if(!dijit._masterTT){ dijit._masterTT = dijit.Tooltip._masterTT = new dijit.Tooltip._MasterTooltip(); }
+            dijit._masterTT._origShow = dijit._masterTT.show;
+            dijit._masterTT.show = this._masterTT_Show;
+            dijit._masterTT._getZoomFactor = () => {return this.gameinterface_zoomFactor;};
         },
 
-        _regOnShowTooltip: function(){
-            const gamegui = this;
-            function onShowTooltip(){
-                const that = this;
-                dijit._masterTT._onHide= function(){
-                    // summary:
-                    //		Called at end of fade-out operation
-                    // tags:
-                    //		protected
-        
-                    this.domNode.style.cssText="";	// to position offscreen again
-                    this.containerNode.innerHTML="";
-                    if(this._onDeck){
-                        // a show request has been queued up; do it now
-                        this.show.apply(this, this._onDeck);
-                        this._onDeck=null;
-                        that.onShow(that._connectNode, that.position);
-                    }
-                };
-                if (dijit._masterTT._onDeck)
-                    return;
-                if (gamegui.gameinterface_zoomFactor == 1) 
-                    return;
-                const zoom = gamegui.gameinterface_zoomFactor;
-                let s = dijit._masterTT.domNode.style;
+        _masterTT_Show: function(innerHTML, aroundNode, position, rtl, textDir, onMouseEnter, onMouseLeave){
+			if(this.aroundNode && this.aroundNode === aroundNode && this.containerNode.innerHTML == innerHTML){
+				return;
+			}
+
+			if(this.fadeOut.status() == "playing"){
+				// previous tooltip is being hidden; wait until the hide completes then show new one
+				this._onDeck=arguments;
+				return;
+			}
+            this._origShow(innerHTML, aroundNode, position, rtl, textDir, onMouseEnter, onMouseLeave);
+            const zoom = this._getZoomFactor();
+            if (zoom == 1)
+                return;
+                // look if aroundNode is a node inside a zoomed div and store this info to not redo that each time
+                if (!aroundNode.hasAttribute('_may_be_zoomed')){
+                // look if 
+                const may_be_zoomed = $('page-content').contains(aroundNode) || $('right-side-first-part').contains(aroundNode) || $('page-title').contains(aroundNode);
+                aroundNode.setAttribute('_may_be_zoomed', may_be_zoomed.toString());
+            }
+            if (aroundNode.getAttribute('_may_be_zoomed')==="true"){
+                this.domNode.style.zoom = zoom; 
+                let s = this.domNode.style;
                 const left = parseInt(s.left.slice(0, -2));
                 const top = parseInt(s.top.slice(0, -2));
-                const width = parseInt(s.width.slice(0, -2));
-                s.left = (left * zoom) + (window.scrollX * (1 -zoom)) + 'px';
-                s.top  = (top * zoom)  + (window.scrollY * (1 -zoom)) + 'px';
-                s.width = (width*zoom)+'px';
-                s = dijit._masterTT.connectorNode.style;
-                if (s.top!=""){
-                    const top2 = parseInt(s.top.slice(0, -2));
-                    s.top  = (top2 * zoom) /* + (window.scrollY * (1 -zoom)) */ + 'px';
-                }
-                if (s.left!=""){
-                    const left2 = parseInt(s.left.slice(0, -2));
-                    s.left = (left2 * zoom) /* + (window.scrollX * (1 -zoom)) */ + 'px';
-                }
-            }
-            return onShowTooltip;
-        },
-        
-        addTooltip: function( id, help, action, delay )
-        {
-            this.inherited(arguments);
-            if ( !this.bHideTooltips )
-                this.tooltips[id].onShow = this._regOnShowTooltip();
-        },
-      
-        addTooltipHtml: function( id, help, action, delay )
-        {
-            this.inherited(arguments);
-            if ( !this.bHideTooltips )
-                this.tooltips[id].onShow = this._regOnShowTooltip();
-        },
-        switchDisplayTooltips: function( mode )
-        {
-            this.inherited(arguments);
-            if ( !this.bHideTooltips )
-            {
-                // Define onShow callback as empty (show tooltips)
-                for (var i in this.tooltips) {
-                    this.tooltips[i].onShow = this._regOnShowTooltip();
-                }
+                const scrollCorr =  (1 -zoom) / zoom;
+                s.left = left + (window.scrollX * scrollCorr) + 'px';
+                s.top  = top + (window.scrollY * scrollCorr) + 'px';
             }
         },
     });
