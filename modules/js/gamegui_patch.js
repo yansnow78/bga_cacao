b271a40ea7eca3edc1a10109616af8bfd8773066
yansnow78 <ygrugni@gmail.com>
2023-04-13 15:56:53 +0200

    improve tootlip patch

--- modules/js/gamegui_patch.js
+++ modules/js/gamegui_patch.js
@@ -9,18 +9,52 @@ function (dojo, declare, domGeometry, Tooltip) {
     return declare("ebg.core.gamegui_patch", null, {
         constructor: function(){
             console.log('ebg.core.gamegui_patch constructor');
-            domGeometry._origPosition = domGeometry.position;
-            domGeometry._getZoomFactor = () => {return this.gameinterface_zoomFactor;};
-            domGeometry._isZoomSupported = (typeof document.body.style.zoom !== "undefined");
-            var vendor = navigator.vendor;
-            domGeometry._isSafariDetected = vendor && (vendor.toLowerCase().indexOf('apple') >= 0);
-            Tooltip._MasterTooltip.prototype._origShow = Tooltip._MasterTooltip.prototype.show;
-            Tooltip._MasterTooltip.prototype.show = this._masterTT_Show;
-            Tooltip._MasterTooltip.prototype._geom_position = this._geom_position;
+            this._checkIfPosCorrNeeded();
+        },
+
+        _checkIfPosCorrNeeded: function(){
+            const scrollX = window.pageXOffset;
+            const scrollY = window.pageYOffset;
+            const el = document.createElement("div");
+            el.style = 'top : 10px; left: 10px; zoom: 2.0; width: 4000px; height: 4000px; position: absolute';
+            document.body.appendChild(el);
+            const el2 = document.createElement("div");
+            el2.style = 'top : 20px; left: 5px; zoom: 4.0; width: 10px; height: 10px; position: absolute';
+            el.appendChild(el2);
+            window.scroll(0,0);
+            const tBox = domGeometry.position(el2);
+            const bCorrPos = (tBox.x==7.5);
+            console.log("_checkIfPosCorrNeeded", bCorrPos, tBox.x);
+            if (bCorrPos){
+                window.scroll(80,0);
+                const tBox2 = domGeometry.position(el2);
+                domGeometry._bCorrScroll = (tBox2.x==-72.5);
+                domGeometry._origPosition = domGeometry.position;
+                domGeometry._getZoom = this._getZoom;
+                Tooltip._MasterTooltip.prototype._origShow = Tooltip._MasterTooltip.prototype.show;
+                Tooltip._MasterTooltip.prototype.show = this._masterTT_Show;
+                Tooltip._MasterTooltip.prototype._geom_position = this._geom_position;
+                Tooltip._checkIfPosCorrNeeded = this._checkIfPosCorrNeeded;
+                console.log("_checkIfPosCorrNeeded", domGeometry._bCorrScroll, tBox2.x);
+            }
+            document.body.removeChild(el);
+            window.scroll(scrollX,scrollY);
+        },
+
+        _getZoom: function(node){
+            let zoom = 1.0;
+            if (typeof node.style.zoom !== "undefined") {
+                zoom = node.style.zoom;
+                if (zoom =="")
+                    zoom = 1.0;
+            }
+            const parent = node.parentElement; 
+            if (parent)
+                zoom = zoom * this._getZoom(parent);
+                return zoom;
         },
 
         _masterTT_Show: function(innerHTML, aroundNode, position, rtl, textDir, onMouseEnter, onMouseLeave){
-            // domGeometry._origPosition = domGeometry.position;
             domGeometry.position = this._geom_position;
             try {
                 this._origShow(innerHTML, aroundNode, position, rtl, textDir, onMouseEnter, onMouseLeave);
@@ -30,32 +64,23 @@ function (dojo, declare, domGeometry, Tooltip) {
         },
 
         _geom_position : function(/*DomNode*/ node, /*Boolean?*/ includeScroll){
-            const zoom = this._getZoomFactor();
-            if ((this._isZoomSupported) && (zoom != 1)) {
-                // look if aroundNode is a node inside a zoomed div and store this info to not redo that each time
-                if (!node.hasAttribute('_may_be_zoomed')){
-                    const may_be_zoomed = $('page-content').contains(node) || 
-                        $('right-side-first-part').contains(node) ||
-                        $('page-title').contains(node);
-                    node.setAttribute('_may_be_zoomed', may_be_zoomed.toString());
-                }
-                if (node.getAttribute('_may_be_zoomed')==="true"){
-                    let position = this._origPosition(node, false);
-                    position.x = position.x*zoom;
-                    position.y = position.y*zoom;
-                    if (includeScroll) {
-                        if (this._isSafariDetected){
-                            position.x += window.pageXOffset*zoom;
-                            position.y += window.pageYOffset*zoom;
-                        } else {
-                            position.x += window.pageXOffset;
-                            position.y += window.pageYOffset;                            
-                        }
+            const zoom = this._getZoom(node);
+            if (zoom != 1) {
+                let position = this._origPosition(node, false);
+                position.x = position.x*zoom;
+                position.y = position.y*zoom;
+                if (includeScroll) {
+                    if (this._bCorrScroll){
+                        position.x += window.pageXOffset*zoom;
+                        position.y += window.pageYOffset*zoom;
+                    } else {
+                        position.x += window.pageXOffset;
+                        position.y += window.pageYOffset;                            
                     }
-                    position.w = position.w*zoom;
-                    position.h = position.h*zoom;
-                    return position;
                 }
+                position.w = position.w*zoom;
+                position.h = position.h*zoom;
+                return position;
             } 
             return this._origPosition(node, includeScroll);
         }
